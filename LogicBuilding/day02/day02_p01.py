ab6a2e39da20fe6089fad129d54f5657344dc6a9
hanson0704 <hansonvaz0704@gmail.com>
2026-03-17 21:04:31 +0530

    Day 2 Code Updates

--- LogicBuilding/day02/day02_p01.py
+++ LogicBuilding/day02/day02_p01.py
@@ -8,7 +8,6 @@ if num%2==0:
     print(f"{num} is an even number")
 else:
     print(f"{num} is an odd number")
-    
 
 '''
 Output:
--- LogicBuilding/day02/day03_p03.py
+++ LogicBuilding/day02/day03_p03.py
@@ -9,7 +9,6 @@ if num%7==0:
     print(f"{num} is a multiple of 7")
 else:
     print(f"{num} is not a multiple of 7")
-    
 
 '''
 Output:
