63f75413e3970e53817589818d8f1ed97e3d7854
Hanson Vaz <68385291+hanson0704@users.noreply.github.com>
2026-04-11 21:52:57 +0530

    Day 18 corrected code

--- LogicBuilding/day18/p03.py
+++ LogicBuilding/day18/p03.py
@@ -7,15 +7,13 @@
 '''
 
 for i in range(1, 6):
-    for j in range(i):
-        print(i + j, end=" ")
+    for j in range(1, i + 1):
+        print(j + (i * (i - 1)) // 2, end=' ')
     print()
 
-'''
-PS D:\CodeMonk\LogicBuilding> python -u "d:\CodeMonk\LogicBuilding\day18\p03.py"
+'''PS D:\CodeMonk> python -u "d:\CodeMonk\LogicBuilding\day18\p03.py"
 1 
 2 3 
-3 4 5 
-4 5 6 7 
-5 6 7 8 9 
-'''
+4 5 6 
+7 8 9 10 
+11 12 13 14 15 '''
